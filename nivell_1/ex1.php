5932fe80815a5ad45fbacb57955beb0ad1025705
Marc <marc.artco@gmail.com>
2022-02-01 18:48:06 +0000

    singleton fin

---
+++ nivell_1/ex1.php
@@ -0,0 +1,42 @@
+<?php
+
+class Tigger
+{
+    private static $instance;
+    private $counter;
+
+    private function __construct()
+    {
+        echo "Building character...".PHP_EOL;
+    }
+
+    public function roar(){
+        echo "Grrr!".PHP_EOL;
+        ++$this->counter;
+    }
+
+    public static function getInstance(){
+        if (!self::$instance instanceof self) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    public function getCounter(){
+        return $this->counter;
+    }
+}
+
+Tigger::getInstance()->roar();
+Tigger::getInstance()->roar();
+Tigger::getInstance()->roar();
+Tigger::getInstance()->roar();
+Tigger::getInstance()->roar();
+Tigger::getInstance()->roar();
+
+echo "Contador = ".Tigger::getInstance()->getCounter().PHP_EOL;
+
+
+
+?>
